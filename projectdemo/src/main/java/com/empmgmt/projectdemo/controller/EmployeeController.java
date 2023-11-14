fe7064c771a8168b98324520d56a4dbcf05a2f2a
TonyAugustineFrank <samstony02@gmail.com>
2023-11-14 10:57:45 +0530

    Linked to database and post implemented

--- projectdemo/src/main/java/com/empmgmt/projectdemo/controller/EmployeeController.java
+++ projectdemo/src/main/java/com/empmgmt/projectdemo/controller/EmployeeController.java
@@ -1,4 +1,22 @@
 package com.empmgmt.projectdemo.controller;
 
+import com.empmgmt.projectdemo.model.Employee;
+import com.empmgmt.projectdemo.service.EmpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
 public class EmployeeController {
+    @Autowired
+    private EmpService empService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Employee employee){
+        empService.saveEmployee(employee);
+        return "New Employee Added";
+    }
 }
--- projectdemo/src/main/java/com/empmgmt/projectdemo/service/EmpServiceImplementation.java
+++ projectdemo/src/main/java/com/empmgmt/projectdemo/service/EmpServiceImplementation.java
@@ -3,7 +3,9 @@ package com.empmgmt.projectdemo.service;
 import com.empmgmt.projectdemo.model.Employee;
 import com.empmgmt.projectdemo.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmpServiceImplementation implements EmpService{
     @Autowired
     private EmployeeRepository employeeRepository;
